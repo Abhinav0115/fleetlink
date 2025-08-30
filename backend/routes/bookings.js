115491e859ba7113668a603f54ddabbffaf6825e
Abhinav Anand <abhinav011501@gmail.com>
2025-08-30 11:44:51 +0530

    add: create routes for bookings and vehicles with CRUD operations

---
+++ backend/routes/bookings.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+    createBooking,
+    deleteBooking,
+    getBooking,
+    getAllBookings,
+} from "../controllers/bookingController.js";
+
+const router = express.Router();
+
+router.post("/", createBooking);
+router.get("/", getAllBookings);
+router.get("/:id", getBooking);
+router.delete("/:id", deleteBooking);
+
+export default router;
