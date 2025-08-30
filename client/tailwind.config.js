bc5c0cdcf647de08f2bb8e6ba2951623d32c36bb
Abhinav Anand <abhinav011501@gmail.com>
2025-08-30 11:39:59 +0530

    Add: initialize client with Vite, React, and Tailwind CSS, including dependencies

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
+
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [tailwindcss],
+};
